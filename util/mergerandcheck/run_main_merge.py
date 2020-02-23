111f2763ecc38a6eabcdff174548426009e40571
YangChen <chenyang199305@hotmail.com>
2020-02-23 23:18:20 +0800

    修改脚本输出方式

--- util/mergerandcheck/run_main_merge.py
+++ util/mergerandcheck/run_main_merge.py
@@ -2,32 +2,20 @@
 # -*- encoding: utf-8 -*-
 
 import os
-import logging
 import sys
+from MergeAndCheck_Main import Merge
 
 WORKSPACE_PATH = '/home/ncp/webhook/'
 REPOSITORY_NAME = 'COVID-19'
 LOCAL_REPOSITORY_PATH = os.path.join(WORKSPACE_PATH, REPOSITORY_NAME)
 CHECK_SCRIPT_PATH = os.path.join(LOCAL_REPOSITORY_PATH, 'util/mergerandcheck')
-CHECK_SCRIPT_NAME = 'MergeAndCheck_Main.py'
-sys.path.append(CHECK_SCRIPT_PATH)
 os.chdir(CHECK_SCRIPT_PATH)
-from MergeAndCheck_Main import Merge
-# reload(Merge)
-ANACONDA_PYTHON_PATH = '/home/ncp/anaconda3/bin/python'
 
 if __name__ == '__main__':
     province = sys.argv[1]
-    # province = 'anhui'
     file_name = sys.argv[2]
-    # file_name = 'anhuiCaseStatistics_20200212.xlsx'
     try:
         check_result, log_file_name = Merge(province, file_name)
-        if check_result == 'Pass':
-            print('Pass')
-        elif check_result is None:
-            print("None")
-        else:
-            print('Fail')
+        print("%s|log_path:%s" % (check_result, log_file_name))
     except Exception as e:
-        logging.error(str(e))
+        print("Fail|%s" % str(e))
